36089aedc1bca6491d3c5fe4c7a80f7dbd8d5e5c
Ramalingam S <codeandgyan@gmail.com>
2024-12-23 11:26:54 +0530

    externalized the port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.all("/", async (req, res) => {
   res.send("Bot is running");
