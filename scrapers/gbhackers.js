ae4394a61c07232a74e31335c245a9bad2d34833
Ramalingam S <codeandgyan@gmail.com>
2025-11-27 23:10:23 +0530

    gbhackers 403 issue fix try # 1

--- scrapers/gbhackers.js
+++ scrapers/gbhackers.js
@@ -11,7 +11,13 @@ const scrapeNews = async (pageNumber = 1) => {
     const { data } = await axios.get(websiteUrl, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Language": "en-US,en;q=0.9",
+        Referer: url,
+        Connection: "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
       },
     });
     const $ = cheerio.load(data);
